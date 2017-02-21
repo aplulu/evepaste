043f7791c4dc2e78f310f280c621832101db6830
Aplulu <aplulu.liv@gmail.com>
2017-02-22 02:40:34 +0900

    setting Beego runmode

--- main/main.go
+++ main/main.go
@@ -7,9 +7,13 @@ import (
 	"fareastdominions.com/evepaste/controllers"
 	"fareastdominions.com/evepaste/template"
 	"fareastdominions.com/evepaste/eve/repository"
+	"google.golang.org/appengine"
 )
 
 func init() {
+	if !appengine.IsDevAppServer() {
+		beegae.BConfig.RunMode = "prod"
+	}
 	repository.LoadTables("data/")
 	template.InitTemplate()
 	controllers.InitController()
